a1e225978f45463baa96d88f1a939e03c28f9f48
chanxxx1912 <chandana1912harna@gmail.com>
2022-07-30 13:51:35 +0530

    first_commit

---
+++ src/main/java/Hello/World/Docker/Test/Controller.java
@@ -0,0 +1,14 @@
+package Hello.World.Docker.Test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	
+	@GetMapping(path = "/")
+	public String hello() {
+		return "Hello World version 2!";
+	}
+
+}
